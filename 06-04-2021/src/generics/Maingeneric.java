4de56c006892e963b391b2acb0da837b81002af5
babji <babji@DESKTOP-2VFRJ0D>
2021-04-07 04:25:50 +0530

    example of generics basics

---
+++ 06-04-2021/src/generics/Maingeneric.java
@@ -0,0 +1,22 @@
+package generics;
+
+public class Maingeneric {
+	public static  <array> void Pleaseprint(array[] inputarray) {
+		for(array Element: inputarray ) {
+			System.out.println(Element);
+		}
+	}
+
+	public static void main(String[] args) {
+		Integer[] intarray = {1,2,3,4,5,6};
+		Double[] doublearray= {1.2,2.3,3.4,4.5,5.6,6.7};
+		Character[] chararray= {'a','b','c','d','e','f'};
+		System.out.println("my int array is");
+        Pleaseprint(intarray);
+        System.out.println("my double array is");
+        Pleaseprint(doublearray);
+        System.out.println("my char array is");
+        Pleaseprint(chararray);
+	}
+
+}
